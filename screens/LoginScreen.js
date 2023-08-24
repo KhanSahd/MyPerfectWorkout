76bed6327ccafdcd7b42227bf749a910d53a4d4b
sahdkhan <03sahd@gmail.com>
2023-08-24 11:47:14 -0700

    Connected to ExerciseDB API

---
+++ screens/LoginScreen.js
@@ -0,0 +1,64 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+} from "react-native-heroicons/solid";
+import { useNavigation } from "@react-navigation/native";
+
+const LoginScreen = () => {
+  const navigation = useNavigation();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    if (email.toUpperCase() === "ADMIN" && password.toUpperCase() === "ADMIN") {
+      navigation.navigate("Home");
+    }
+  };
+
+  return (
+    <SafeAreaView className="bg-[#FF5C00] flex-1 relative items-center justify-center ">
+      <TouchableOpacity
+        onPress={() => navigation.goBack()}
+        className="absolute top-14 left-5 "
+      >
+        <ChevronLeftIcon size={40} color="white" />
+      </TouchableOpacity>
+
+      <View className="space-y-20">
+        <Text className="text-4xl font-bold text-white text-center">Login</Text>
+
+        <View>
+          <TextInput
+            onChangeText={(e) => setEmail(e)}
+            className="border-white text-white p-4 w-screen text-center text-2xl font-bold"
+            placeholder="Email"
+          />
+          <TextInput
+            onChangeText={(e) => setPassword(e)}
+            placeholder="Password"
+            className="border-white text-white p-4 w-screen text-center text-2xl font-bold"
+            secureTextEntry={true}
+          />
+        </View>
+      </View>
+
+      <TouchableOpacity
+        onPress={() => handleLogin()}
+        className="absolute bottom-14 right-5 bg-white rounded-full p-2"
+      >
+        <ChevronRightIcon size={40} color="#FF5C00" />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,33 @@
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+
+const WelcomeScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <SafeAreaView className="flex-1 bg-[#FF5C00] items-center relative">
+      <Text className="text-4xl font-bold text-white mt-24 w-screen text-center px-20">
+        The Perfect Gym Routine
+      </Text>
+
+      <View className="justify-center items-center space-y-10 bg-white h-96 w-96 absolute -bottom-20 -right-20 rounded-full">
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Login")}
+          className="bg-[#FF5C00] px-14 py-3 text-lg rounded-full mr-10"
+        >
+          <Text className="text-center text-white text-lg font-bold">
+            Login
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="px-11 py-3 text-lg rounded-full border-2 border-[#FF5C00] mr-10 mb-10">
+          <Text className="text-center text-[#FF5C00] text-lg font-bold">
+            Register
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default WelcomeScreen;
