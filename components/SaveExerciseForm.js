09f74f5f1f1d3a8b643beb168f01ae055f7aedf7
sahdkhan <03sahd@gmail.com>
2023-10-22 23:49:31 -0700

    Added Save Menu

--- components/SaveExerciseForm.js
+++ components/SaveExerciseForm.js
@@ -1,10 +1,22 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 
 const SaveExerciseForm = () => {
+  const [exerciseName, setExerciseName] = useState('');
+
   return (
-    <View className="m-auto ">
-      <Text>Form</Text>
+    <View className="m-auto bg-red-600 mx-16 rounded-full">
+      <View className="flex flex-col justify-center items-center">
+        <TextInput
+          onChangeText={(e) => setExerciseName(e)}
+          className=" text-white p-4 text-center text-2xl font-bold"
+          placeholder="exercise name"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity className="bg-white mb-2 p-4 rounded-full w-1/2">
+          <Text className="text-center font-bold uppercase">Save</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
--- components/SaveExerciseMenu.js
+++ components/SaveExerciseMenu.js
@@ -1,18 +1,11 @@
-import {
-  View,
-  Text,
-  SafeAreaView,
-  TouchableOpacity,
-  StyleSheet,
-  ScrollView,
-} from "react-native";
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import * as Animatable from "react-native-animatable";
-import { PlusIcon } from "react-native-heroicons/solid";
-import { useNavigation } from "@react-navigation/native";
-import Backbutton from "./Backbutton";
-import SaveExerciseForm from "./SaveExerciseForm";
+import { View, Text, SafeAreaView, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import * as Animatable from 'react-native-animatable';
+import { PlusIcon, ChevronDownIcon } from 'react-native-heroicons/solid';
+import { useNavigation } from '@react-navigation/native';
+import Backbutton from './Backbutton';
+import SaveExerciseForm from './SaveExerciseForm';
 
 const SaveExerciseMenu = () => {
   const isMenuOpen = useSelector((state) => state.saveMenu.menuShown);
@@ -20,16 +13,15 @@ const SaveExerciseMenu = () => {
   const [showForm, setShowForm] = useState(false);
 
   return (
-    <ScrollView className="w-full h-full bg-[#D0CCD0]">
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Text className="absolute right-0 top-5">X</Text>
+    <ScrollView className="w-full h-full bg-white">
+      <TouchableOpacity className="absolute right-5 top-5 " onPress={() => navigation.goBack()}>
+        <ChevronDownIcon color={'black'} size={30} />
       </TouchableOpacity>
       {/* Create a new workout */}
       <View className="m-7 ml-3 items-center w-28 ">
         <TouchableOpacity
-          onPress={() => setShowForm(true)}
-          className="bg-white border border-black rounded-full w-12 h-12 justify-center items-center"
-        >
+          onPress={() => setShowForm(!showForm)}
+          className="bg-white border border-black rounded-full w-12 h-12 justify-center items-center">
           <PlusIcon />
         </TouchableOpacity>
         <Text className="text-center mt-2">Create new workout</Text>
@@ -41,16 +33,5 @@ const SaveExerciseMenu = () => {
     </ScrollView>
   );
 };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     width: "100",
-//     display: flex,
-//     justifyContent: "center",
-//     backgroundColor: "#D0CCD0",
-//     borderTopLeftRadius: "1rem" /* 16px */,
-//     borderTopRightRadius: "1rem" /* 16px */,
-//   },
-// });
 
 export default SaveExerciseMenu;
