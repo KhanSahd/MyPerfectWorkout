138f5c2aa0fa9b26e41f787029573bcbf27bc60f
sahdkhan <03sahd@gmail.com>
2023-10-22 23:05:51 -0700

    Redux for auth

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,39 +1,39 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
-import React, { useEffect, useState } from "react";
-import CategoryButton from "../components/CategoryButton";
-import { XRAPIDAPIKEY, XRAPIDAPIHOST } from "@env";
-import { useSelector, useDispatch } from "react-redux";
-import { add, fetchExercises } from "../features/exercises/exercisesSlice";
-import { logout, reset } from "../features/auth/authSlice";
-import { useNavigation } from "@react-navigation/native";
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import CategoryButton from '../components/CategoryButton';
+import { XRAPIDAPIKEY, XRAPIDAPIHOST } from '@env';
+import { useSelector, useDispatch } from 'react-redux';
+import { add, fetchExercises } from '../features/exercises/exercisesSlice';
+import { logout, reset } from '../features/auth/authSlice';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Categories = () => {
   const dispatch = useDispatch();
   const navigation = useNavigation();
 
   const { user } = useSelector((state) => state.auth);
-  const exercises = useSelector((state) => state.exercises.exercises);
+  // const exercises = useSelector((state) => state.exercises.exercises);
+  // const isLoggedIn = AsyncStorage.getItem('user');
 
   const onLogout = () => {
     dispatch(logout());
     dispatch(reset());
-    navigation.navigate("Welcome");
+    navigation.navigate('Welcome');
   };
 
   useEffect(() => {
+    console.log('user', user);
     dispatch(fetchExercises());
   }, []);
 
   return (
     <SafeAreaView className="flex-1 bg-[#F02D3A]">
-      <TouchableOpacity
-        onPress={() => onLogout()}
-        className="absolute top-14 right-5"
-      >
+      <TouchableOpacity onPress={() => onLogout()} className="absolute top-14 right-5">
         <Text className="text-white text-xl">Logout</Text>
       </TouchableOpacity>
       <Text className="text-center text-2xl mt-10 text-white font-bold">
-        Pick a category to find a workout
+        Welcome {user.name}, Pick a category to find a workout
       </Text>
       <View className="flex-1 flex-col justify-evenly items-center mt-5">
         <CategoryButton text="Body Part" page="BodyParts" />
